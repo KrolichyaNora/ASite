5b3fed8c604a0a483d51ea308ef9cf9b7c23a98c
Мурза Лейн <murza8965@gmail.com>
2022-09-27 01:35:33 +0300

    Do not allow empty login or password in DB

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,5 +1,10 @@
+from importlib.metadata import requires
+from urllib import request
 from django.db import models
+from django.db.models import CheckConstraint, Q, F
 
+# Q - фильтр
+# F - обращение к полю в модели
 class Role(models.Model):
     name = models.CharField(max_length=32)
 
@@ -10,3 +15,16 @@ class User(models.Model):
 
     def __str__(self):
         return f"({self.role}) {self.login}: {self.password}"
+
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                check = ~Q(login = ""), 
+                name = 'login_isnt_empty',
+            ),
+            CheckConstraint(
+                check = ~Q(password = ""), 
+                name = 'password_isnt_empty',
+            ),
+        ]
+
